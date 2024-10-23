e4c78bccb55a0291b8c6ba0100081b385df025e3
Rodrigo Carvalho <rhrc09@gmail.com>
2024-10-23 13:48:08 -0300

    refactor: update register validator

--- src/auth/auth.validators.ts
+++ src/auth/auth.validators.ts
@@ -4,13 +4,13 @@ import { runValidations } from "../utils";
 export const registerValidator = runValidations([
   body("name").trim().notEmpty().withMessage("Name is required."),
   body("email")
+    .trim()
     .notEmpty()
     .withMessage("Email is required.")
     .isEmail()
     .withMessage("Email is not valid."),
   body("password")
-    .notEmpty()
-    .withMessage("Password is required")
+    .trim()
     .isLength({ min: 8 })
     .withMessage("Password must contain at least 8 characters"),
 ]);
